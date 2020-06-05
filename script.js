7ca61d7b9b2072256a4619da8f59417d3ed9fdf9
Andrei Yudin <at-4@mail.ru>
2020-06-05 15:13:36 +0300

    Lesson 02 completed

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 70000;
+let income = 'купоны по облигациям';
+let addExpenses = 'Связь, Коммуналка, Продукты, Транспорт';
+let deposit = true;
+let mission = 5000000;
+let period = 12;
+let budgetDay = money / 30;
 
-alert('Hello World!');
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
 
-console.log('Привет мир');
+console.log(addExpenses.length);
+
+console.log(`Период равен ${period} месяцев`);
+console.log(`Цель заработать ${mission} шри-ланкийских рупий`);
+
+console.log(addExpenses.toLowerCase().split(', '));
+
+console.log(budgetDay);
