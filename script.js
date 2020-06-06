d6727bf2f68e62f095f521bf8eb9350105552f17
Andrei Yudin <at-4@mail.ru>
2020-06-06 17:22:17 +0300

    Used reduce method instead of for loop

--- script.js
+++ script.js
@@ -4,12 +4,9 @@ const num = 266219;
 
 function multiplyDigits(number) {
     const arr = number.toString().split('');
-    let result = arr[0];
-    
-    for (let i = 1; i < arr.length; i++) {
-        result *= arr[i];
-    }
-    
+    const reducer = (accumulator, currentValue) => accumulator * currentValue;    
+    let result = arr.reduce(reducer);
+
     console.log(result);
     result = (result ** 3).toString();
     alert('Первые две цифры результата: ' + result[0] + result[1]);
