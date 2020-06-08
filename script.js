f1cfd43d337dd1646a065e049ba3161204cd49ec
Andrei Yudin <at-4@mail.ru>
2020-06-08 21:20:29 +0300

    Lesson 03 completed

--- script.js
+++ script.js
@@ -18,3 +18,51 @@ console.log(`Цель заработать ${mission} шри-ланкийски�
 console.log(addExpenses.toLowerCase().split(', '));
 
 console.log(budgetDay);
+
+money = +prompt('Ваш месячный доход?', '');
+
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', '');
+
+deposit = confirm('Есть ли у вас депозит в банке?');
+
+let expenses1 = prompt('Введите обязательную статью расходов?', '');
+let amount1 = +prompt('Во сколько это обойдется?', '');
+
+let expenses2 = prompt('Введите обязательную статью расходов?', '');
+let amount2 = +prompt('Во сколько это обойдется?', '');
+
+let budgetMonth = money - (amount1 + amount2);
+
+if (isNaN(money) || money < 0) {
+    console.log('Что-то пошло не так');
+} else {
+    console.log('Бюджет на месяц: ' + budgetMonth);
+}
+
+period = mission / budgetMonth;
+
+if (isNaN(period) || period < 0 || period === Infinity) {
+    console.log('Что-то пошло не так');
+} else {
+    console.log('Цель будет достингута за ' + Math.ceil(period) + ' месяцев');
+}
+
+budgetDay = budgetMonth / 30;
+
+if (isNaN(period) || period < 0) {
+    console.log('Что-то пошло не так');
+} else {
+    console.log('Бюджет на день: ' + Math.floor(budgetDay));
+}
+
+if (budgetDay >= 1200) {
+    console.log('У вас высокий уровень дохода');
+} else if (budgetDay >= 600 && budgetDay < 1200) {
+    console.log('У вас средний уровень дохода');
+} else if (budgetDay > 0 && budgetDay < 600) {
+    console.log('У вас уровень дохода ниже среднего');
+} else if (budgetDay === 0) {
+    console.log('У вас нет дохода');
+} else {
+    console.log('Что-то пошло не так');
+}
