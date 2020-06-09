d10f905427bc0aa075e996b85bb367c684fb739d
Andrei Yudin <at-4@mail.ru>
2020-06-09 22:32:40 +0300

    Рефакторинг кода + period больше не запрашивается у пользователя

--- script.js
+++ script.js
@@ -1,53 +1,32 @@
-let money = 70000;
+let money = +prompt('Ваш месячный доход?', '');
 let income = 'купоны по облигациям';
-let addExpenses = 'Связь, Коммуналка, Продукты, Транспорт';
-let deposit = true;
+let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', '');
+let deposit = confirm('Есть ли у вас депозит в банке?');
 let mission = 5000000;
 let period = 12;
-let budgetDay = money / 30;
+let expenses1 = prompt('Введите обязательную статью расходов?', '');
+let amount1 = +prompt('Во сколько это обойдется?', '');
+let expenses2 = prompt('Введите обязательную статью расходов?', '');
+let amount2 = +prompt('Во сколько это обойдется?', '');
+let budgetMonth = money - (amount1 + amount2);
+let budgetDay = budgetMonth / 30;
 
 console.log(typeof money);
 console.log(typeof income);
 console.log(typeof deposit);
 
 console.log(addExpenses.length);
-
 console.log(`Период равен ${period} месяцев`);
 console.log(`Цель заработать ${mission} шри-ланкийских рупий`);
-
 console.log(addExpenses.toLowerCase().split(', '));
 
-console.log(budgetDay);
-
-money = +prompt('Ваш месячный доход?', '');
-
-addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', '');
-
-deposit = confirm('Есть ли у вас депозит в банке?');
-
-let expenses1 = prompt('Введите обязательную статью расходов?', '');
-let amount1 = +prompt('Во сколько это обойдется?', '');
-
-let expenses2 = prompt('Введите обязательную статью расходов?', '');
-let amount2 = +prompt('Во сколько это обойдется?', '');
-
-let budgetMonth = money - (amount1 + amount2);
+console.log('Цель будет достингута за ' + Math.ceil(mission / budgetMonth) + ' месяцев');
 
 if (isNaN(money) || money < 0) {
     console.log('Что-то пошло не так');
 } else {
     console.log('Бюджет на месяц: ' + budgetMonth);
 }
-
-period = mission / budgetMonth;
-
-if (isNaN(period) || period < 0 || period === Infinity) {
-    console.log('Что-то пошло не так');
-} else {
-    console.log('Цель будет достингута за ' + Math.ceil(period) + ' месяцев');
-}
-
-budgetDay = budgetMonth / 30;
 
 if (isNaN(period) || period < 0) {
     console.log('Что-то пошло не так');
