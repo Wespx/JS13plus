78d100fba8036c4acfbb90a6b50bda354f732de8
Andrei Yudin <at-4@mail.ru>
2020-06-10 17:57:50 +0300

    Фикс бага, при котором из строки могли быть убраны символы до 30, + в консоль добавил вызов функции для теста

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ function handleString(str) {
     if (typeof str !== 'string') {
         alert('В качестве аргумента передана не строка');
     } else if (str.length > 30) {
-        return str.trim().replace(str.substring(30), '...');
+        return str.trim().slice(0, 30) + '...';
     } else {
         return str.trim();
     }
 }
+
+console.log(handleString('123456789012345678901234567890123'));
