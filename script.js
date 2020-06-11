ee30ae77ba3adb11ad6dade7e960f5698e79b657
Andrei Yudin <at-4@mail.ru>
2020-06-11 18:29:08 +0300

    Рефакторинг, добавлена обертка для функции, выводящей в консоль числа из массива, начинающиеся с цифр 2 и 4

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
 'use strict';
 
-let arr = [];
+let getNumbersStartingAt2Or4 = function() {
+    let arr = [];
 
-let pushRandomNumbers = function() {
-    let randomNumber = function() {
-       return Math.floor(Math.random() * 10000).toString();
-    };
-    
-    for (let i = 0; i < 5; i++) {
-        arr.push(randomNumber());
-    }
-
-    arr.push('2' + randomNumber().slice(0, -1));
-    arr.push('4' + randomNumber().slice(0, -1));
-};
-
-let getCertainNumbers = function() {
-    arr.forEach(number => {
-        if (number[0] === '2' || number[0] === '4') {
-            console.log(number);
+    let pushRandomNumbers = function() {
+        let randomNumber = function() {
+        return Math.floor(Math.random() * 10000).toString();
+        };
+        
+        for (let i = 0; i < 5; i++) {
+            arr.push(randomNumber());
         }
-    });
-};
 
-pushRandomNumbers();
-getCertainNumbers();
+        arr.push('2' + randomNumber().slice(0, -1));
+        arr.push('4' + randomNumber().slice(0, -1));
+    };
+
+    let getCertainNumbers = function() {
+        arr.forEach(number => {
+            if (number[0] === '2' || number[0] === '4') {
+                console.log(number);
+            }
+        });
+    };
+
+    pushRandomNumbers();
+    getCertainNumbers();
+};
 
 let getPrimeNumbers = function() {
     for (let i = 2; i <= 100; i++) {
@@ -42,5 +44,6 @@ let getPrimeNumbers = function() {
     }
 };
 
+getNumbersStartingAt2Or4();
 getPrimeNumbers();
 
